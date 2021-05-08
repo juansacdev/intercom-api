936a85227052896b8fbe15309eb2b15e751cb304
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-05-08 17:22:38 -0500

    create routes

---
+++ src/api/lib/routes.js
@@ -0,0 +1,14 @@
+const user = require('../components/User/network')
+const message = require('../components/Message/network')
+const chat = require('../components/Chat/network')
+const swaggerUi = require ('swagger-ui-express')
+const swaggerDoc = require('../../public/swagger.json')
+
+const routes = server => {
+    server.use('/api/users', user)
+    server.use('/api/messages', message)
+    server.use('/api/chats', chat)
+    server.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+}
+
+module.exports = routes
